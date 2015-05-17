10168ef7a1d919943da8b4fe1eb9aa08f3238a39
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-17 14:15:42 -0500

    Show hardcoded stations

---
+++ client/templates/stations/stations_list.js
@@ -0,0 +1,14 @@
+var stationsData = [
+  {
+    name: 'Introducing Telescope'
+  },
+  {
+    name: 'Meteor'
+  },
+  {
+    name: 'The Meteor Book'
+  }
+];
+Template.stationsList.helpers({
+  stations: stationsData
+});
