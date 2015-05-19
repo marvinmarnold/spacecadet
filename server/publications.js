21866961a6e6b017deeabab643b495c2f3904b48
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-18 21:59:32 -0500

    Load pads for station

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,7 @@ Meteor.publish('stations', function() {
   return Stations.find();
 });
 
-Meteor.publish('pads', function() {
-  return Pads.find();
+Meteor.publish('pads', function(stationId) {
+  check(stationId, String);
+  return Pads.find({stationId: stationId});
 });
