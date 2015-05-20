965708bc5ad69d14a3acea243a6cafa461c424d5
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-20 12:07:06 -0500

    Unify header

---
+++ client/unity-js/09-shop.app.js
@@ -0,0 +1,11 @@
+ Meteor.startup(function() {
+ /*
+ * Template Name: Unify - Responsive Bootstrap Template
+ * Description: Business, Corporate, Portfolio, E-commerce and Blog Theme.
+ * Version: 1.6
+ * Author: @htmlstream
+ * Website: http://htmlstream.com
+*/
+
+
+});
