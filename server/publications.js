1a048360cc6104d228f6280faabf670ab691fa23
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-20 18:07:33 -0500

    Functionality added back into UI

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,11 @@ Meteor.publish('stations', function(options) {
   return Stations.find({}, options);
 });
 
+Meteor.publish('singleStation', function(id) {
+  check(id, String);
+  return Stations.find(id);
+})
+
 Meteor.publish('pads', function(stationId) {
   check(stationId, String);
   return Pads.find({stationId: stationId});
