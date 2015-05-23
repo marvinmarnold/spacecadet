5c0b466eeaa064db4964d3f052c002e00a3ccb0e
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-23 15:35:50 -0500

    Shopping cart page

--- client/templates/carts/cart.js
+++ client/templates/carts/cart.js
@@ -79,24 +79,6 @@ Template.CartPayNow.rendered = function(){
 };
 
 
-Router.route('/cart', function () {
-  this.render('CartItems', {
-    data: function () {
-    	var query = {};
-		if(Meteor.userId())
-			query.userId = Meteor.userId();
-		else
-			query.deviceId = Session.get('Cart-deviceId');
-
-		return {
-    		items:Cart.Items.find(query),
-    		hasItems:!Session.equals('Cart-itemCount', 0),
-    		itemCount:Session.get('Cart-itemCount'),
-			itemTotal:Session.get('Cart-itemTotal')
-    	};
-    }
-  });
-});
 
 Template.CartItem.events({
 	'click .remove':function(event, template){
