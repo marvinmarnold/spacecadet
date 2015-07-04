7d292cf5a6f67c51a71b0b0c6a0fd12af2a47492
Marvin Arnold <marvinmarnold@gmail.com>
2015-07-03 22:49:31 -0500

    Billing working

--- client/templates/dockings/new_docking.js
+++ client/templates/dockings/new_docking.js
@@ -1,6 +1,6 @@
 Template.newDocking.helpers({
   total: function() {
-    return 100;
+    return this.price * dockingPeriod();
   },
   startDockingOn: function() {
     return moment(Session.get('startDockingOn')).format('ddd, MMM D, YYYY');
@@ -9,6 +9,38 @@ Template.newDocking.helpers({
     return moment(Session.get('endDockingOn')).format('ddd, MMM D, YYYY');
   },
   dockingPeriod: function() {
-    return moment(Session.get('endDockingOn')).diff(moment(Session.get('startDockingOn')), 'days') + " days";
+    return dockingPeriod() + " days";
   }
+});
+var dockingPeriod = function() {
+  return moment(Session.get('endDockingOn')).diff(moment(Session.get('startDockingOn')), 'days');
+}
+
+Template.newDocking.events({
+  'submit form': function(event, template){
+    event.preventDefault();
+
+    var ccNum = $(event.target).find('[id=cardnumber]').val();
+    var expMo = $(event.target).find('[id=expirationmonth]').val();
+    var expYr = $(event.target).find('[id=expirationyear]').val();
+    var cvc = $(event.target).find('[id=cvc]').val();
+
+    console.log(event);
+    console.log(template.data.pad._id);
+    Stripe.card.createToken({
+        number: ccNum,
+        cvc: cvc,
+        exp_month: expMo,
+        exp_year: expYr,
+    }, function(status, response) {
+        stripeToken = response.id;
+        Meteor.call('chargeCard', stripeToken, template.data.pad._id, Session.get('startDockingOn'),Session.get('endDockingOn'), function(error, dockingId) {
+            if(error) {
+                alert(JSON.stringify(error));
+            } else {
+                Router.go('docking', {_id: dockingId});
+            }
+        });
+    });
+  },
 });
--- server/dockings.js
+++ server/dockings.js
@@ -1,10 +1,41 @@
 Meteor.methods({
-  'chargeCard': function(stripeToken, padId, dockingStartsOn, dockingEndsOn) {
+  'chargeCard': function(stripeToken, padId, startDockingOn, endDockingOn) {
     check(padId, String);
-    check(dockingStartsOn, Date);
-    check(dockingEndsOn, Date);
+    check(startDockingOn, Date);
+    check(endDockingOn, Date);
 
+    var userId = this.userId;
+    var pad = Pads.findOne(padId);
+    var station = Stations.find(pad.stationId)
     var Stripe = StripeAPI(Meteor.settings.stripe_sk);
-    var days = moment(dockingStartsOn).diff(moment(dockingEndsOn), 'days');
+    var days = moment(endDockingOn).diff(moment(startDockingOn), 'days');
+    var total = pad.price * days;
+
+    var charge = Meteor.wrapAsync(Stripe.charges.create, Stripe.charges);
+
+    try {
+      charge({
+        amount: total * 100,
+        currency: 'usd',
+        source: stripeToken
+      });
+
+      dockingId = Dockings.insert({
+        userId: userId,
+        total: total,
+        createdAt: new Date(),
+        padId: pad._id,
+        stationId: station._id,
+        padName: pad.name,
+        stationName: station.name,
+        startDockingOn: startDockingOn,
+        endDockingOn: endDockingOn
+      });
+
+      return dockingId;
+
+    } catch (error) {
+      throw new Meteor.Error("stripe-charge-error", error.message);
+    }
   }
 });
