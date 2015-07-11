98c04e88d0c3d1c05cf2cfc59fd88fe48efeea91
Marvin Arnold <marvinmarnold@gmail.com>
2015-07-10 22:09:13 -0500

    remove station

--- client/templates/stations/stations.js
+++ client/templates/stations/stations.js
@@ -2,4 +2,11 @@ Template.stations.events({
   'click #new-station-button': function(event) {
       Router.go('newStation');
   },
+  'click .delete-station-button': function(event) {
+    event.preventDefault();
+    Meteor.call('deleteStation', this._id, function(error, result) {
+      if (error)
+        return alert(error.reason);
+    });
+  },
 });
