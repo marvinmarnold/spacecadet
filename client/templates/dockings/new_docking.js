b700ac18518e9c3cfc04d4da341234bae4ac3686
Marvin Arnold <marvinmarnold@gmail.com>
2015-07-13 02:29:49 -0500

    Successfully charge tenant on landlord approval

--- client/templates/dockings/new_docking.js
+++ client/templates/dockings/new_docking.js
@@ -34,25 +34,51 @@ Template.newDocking.events({
     var cardholder = $(event.target).find('[id=cardholder]').val();
 
     Stripe.card.createToken({
-        number: ccNum,
-        cvc: cvc,
-        exp_month: expMo,
-        exp_year: expYr,
+      number: ccNum,
+      cvc: cvc,
+      exp_month: expMo,
+      exp_year: expYr,
     }, function(status, response) {
-        stripeToken = response.id;
-        Meteor.call('chargeCard',
-          stripeToken,
-          template.data.pad._id,
-          Session.get('startDockingOn'),
-          Session.get('endDockingOn'),
-          cardholder,
-          function(error, dockingId) {
+      if(response.error) {
+        // console.log("card token not created");
+        alert(response.error);
+      } else {
+        // console.log("success");
+        var storeableCard = {
+          token: response.id,
+          cardholder: cardholder,
+          country: response.card.country,
+          expMo: response.card.exp_month,
+          expYr: response.card.exp_year,
+          last4: response.card.last4,
+          brand: response.card.brand
+        };
+        Meteor.call('createCreditCard',
+          storeableCard,
+          function(error, cardId) {
             if(error) {
-                alert(JSON.stringify(error));
+              // console.log("card not created")
+              alert(JSON.stringify(error));
             } else {
-                Router.go('docking', {_id: dockingId});
+              Meteor.call('createDocking',
+                template.data.pad._id,
+                Session.get('startDockingOn'),
+                Session.get('endDockingOn'),
+                cardholder,
+                cardId,
+                function(error, dockingId) {
+                  if(error) {
+                    // console.log("docking not created");
+                    alert(JSON.stringify(error));
+                  } else {
+                    Router.go('docking', {_id: dockingId});
+                  }
+              });
             }
         });
+      }
     });
   },
 });
+
+// var createCreditCard = function()
--- server/dockings.js
+++ server/dockings.js
@@ -1,76 +1,24 @@
 Meteor.methods({
-  'chargeCard': function(stripeToken,
-    padId,
-    startDockingOn,
-    endDockingOn,
-    dockerName) {
-
-    check(padId, String);
-    check(startDockingOn, Date);
-    check(endDockingOn, Date);
-    check(stripeToken, String);
-    check(dockerName, String)
-
-    var userId = this.userId;
-    var pad = Pads.findOne(padId);
-    var station = Stations.findOne(pad.stationId)
+  'chargeCard': function(dockingId) {
+    check(dockingId, String);
     var Stripe = StripeAPI(Meteor.settings.stripe_sk);
-
-    // avoid any weird race condition where price changes
-    // store all variables before use
-    var padPrice = pad.price;
-    var displayPadPrice = pad.displayPrice;
-    var days = moment(endDockingOn).diff(moment(startDockingOn), 'days') + 1;
-    var spacecadetServiceFee = Meteor.settings.public.spacecadetServiceFee;
-    var spacecadetConnectionFee = Meteor.settings.spacecadetConnectionFee;
-
-    var landlordCutPennies = Math.floor(padPrice * days * 100);
-    var connectionFeePennies = Math.floor(landlordCutPennies * spacecadetConnectionFee);
-    var serviceFeePennies = Math.floor((landlordCutPennies + connectionFeePennies) * spacecadetServiceFee);
-    var pennies = landlordCutPennies + connectionFeePennies + serviceFeePennies;
-
-    var total = pennies / 100
-    var landlordCut = landlordCutPennies / 100;
-    var connectionFee = connectionFeePennies / 100;
-    var serviceFee = serviceFeePennies / 100;
-
     var charge = Meteor.wrapAsync(Stripe.charges.create, Stripe.charges);
 
+    var docking = Dockings.findOne(dockingId);
+    var card = Cards.findOne(docking.chargeToCardId);
+
+    console.log("charge");
+    console.log(docking);
+    console.log(card);
+
+    // Charge tenant
     try {
       charge({
-        amount: pennies,
+        amount: (docking.total * 100).toFixed(0),
         currency: 'usd',
-        source: stripeToken
+        source: card.token
       });
-
-      dockingId = Dockings.insert({
-        userId: userId,
-        dockerName: dockerName,
-        landlordId: userId,
-        total: total,
-        serviceFee: serviceFee,
-        connectionFee: connectionFee,
-        landlordCut: landlordCut,
-        createdAt: new Date(),
-        padId: pad._id,
-        dailyPadPrice: displayPadPrice,
-        stationId: station._id,
-        previewPath: station.previewPath,
-        bannerPath: station.bannerPath,
-        padName: pad.name,
-        stationName: station.name,
-        address: station.address,
-        city: station.city,
-        zip: station.zip,
-        startDockingOn: startDockingOn,
-        endDockingOn: endDockingOn,
-        state: Dockings.state_awaiting_landlord_approval
-      });
-
-      Meteor.call('sendLandlordApprovalEmail', Meteor.user().emails[0].address);
-
-      return dockingId;
-
+        console.log("charged");
     } catch (error) {
       throw new Meteor.Error("stripe-charge-error", error.message);
     }
