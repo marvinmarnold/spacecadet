5f206769696a773446c30b07f9450d7a338c9a12
Marvin Arnold <marvinmarnold@gmail.com>
2015-07-16 18:42:27 -0500

    Remove debug logs

--- server/dockings.js
+++ server/dockings.js
@@ -1,13 +1,11 @@
 Meteor.methods({
   'chargeCard': function(dockingId) {
-    console.log("chargeCard");
     check(dockingId, String);
     var Stripe = StripeAPI(Meteor.settings.stripe_sk);
     var charge = Meteor.wrapAsync(Stripe.charges.create, Stripe.charges);
 
     var docking = Dockings.findOne(dockingId);
     if(managesDocking(docking) && isAwaitingPayment(docking)) {
-      console.log("chargeCard is manager");
       var card = Cards.findOne(docking.chargeToCardId);
 
       // Charge tenant
@@ -17,14 +15,12 @@ Meteor.methods({
           currency: 'usd',
           source: card.token
         });
-        console.log("chargeCard finished");
       } catch (error) {
         throw new Meteor.Error("stripe-charge-error", error.message);
       }
     }
   },
   'creditLandlord': function(dockingId) {
-    console.log("creditLandlord");
     check(dockingId, String);
     var docking = Dockings.findOne(dockingId);
 
@@ -44,26 +40,6 @@ Meteor.methods({
       });
 
       Dockings.update(dockingId, {$set: {state: Dockings.state_payments_settled}});
-
-      // var bank = Banks.findOne({userId: docking.landlordId, isDefault: true});
-
-      // var externalAccount = {
-      //   "country": "US",
-      //   "currency": "USD",
-      //   "account_number": bank.accountNumber,
-      //   "routing_number": bank.routingNumber,
-      // }
-
-      // console.log("creditLandlord recipient");
-      // var transfer = Stripe.transfers.create({
-      //   amount: (docking.landlordCut * 100).toFixed(0),
-      //   currency: "usd",
-      //   recipient: bank.stripe_id,
-      //   description: "JULY SALES"
-      // }, function(err, transfer) {
-      //   if (err) throw new Meteor.Error("stripe-charge-error-transfer", err);
-      //   console.log("creditLandlord is manager");
-      // });
     }
   }
 });
