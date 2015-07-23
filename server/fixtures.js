a391354a5e4fe330a26ed662fc54835e7dcc5eef
Marvin Arnold <marvinmarnold@gmail.com>
2015-07-23 04:06:59 -0500

    Remove dev seeds for prod deployment

--- server/fixtures.js
+++ server/fixtures.js
@@ -10,78 +10,18 @@ if (States.find().count() === 0) {
   }
 }
 
-var landlordId;
 
 if ( Meteor.users.find().count() === 0 ) {
-  landlordId = Accounts.createUser({
-    username: "testlandlord",
-    email: Meteor.settings.landlordEmail,
-    password: 'spacecadet',
-    profile: {
-      entityName: "SpaceCadet",
-      firstName: 'Test',
-      lastName: "Landlord",
-      company: "Test Company",
-      phoneNumber: "3016864576",
-      role: Accounts.role.landlord
-    }
-  });
-  // landlord = Meteor.users.findOne(landlordId);
-
   Accounts.createUser({
-    username: "testadmin",
+    username: Meteor.settings.adminUsername,
     email: Meteor.settings.adminEmail,
-    password: 'spacecadet',
+    password: Meteor.settings.adminPassword,
     profile: {
       entityName: "SpaceCadet",
-      firstName: 'Test',
-      lastName: "Admin",
-      phoneNumber: "3016864576",
+      firstName: 'Steven',
+      lastName: "Quintanilla",
+      phoneNumber: "6178697585",
       role: Accounts.role.admin
     }
   });
-
-  Accounts.createUser({
-    username: "testtenant",
-    email: Meteor.settings.tenantEmail,
-    password: 'spacecadet',
-    profile: {
-      entityName: "SpaceCadet",
-      firstName: 'Test',
-      lastName: "Tenant",
-      phoneNumber: "3016864576",
-      role: Accounts.role.tenant
-    }
-  });
-}
-
-if (Stations.find().count() === 0) {
-  for (var i = 0; i < 6; i++) {
-    var stationId = Stations.insert({
-      name: 'Test station #' + i,
-      description: "Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby.",
-      address: "1307 Oretha Castle Haley Blvd, Suite 303M",
-      city: "New Orleans",
-      zip: "70113",
-      state: "LA",
-      userId: landlordId,
-      previewPath: "/station.jpg",
-      bannerPath: "/station-banner.jpg"
-    });
-
-    for (var j = 1; j < 6; j++) {
-      var padId = Pads.insert({
-        name: 'Landing pad #' + (i + 2 *j),
-        size: "2x2",
-        price: (i*10 + j*100),
-        numAvailable: (i + j),
-        description: "Some type of description",
-        stationId: stationId,
-        imagePath: "/pad.jpg",
-        occupancy: (i + j*10),
-        availabilityStarts: new Date(),
-        availabilityEnds: new Date()
-      });
-    }
-  }
 }
